04f1350dd941d9063720ea08fe5b6f25527f555a
Aaron H <aaron.hossain14@gmail.com>
2026-09-17 09:25:40 -0400

    Completed Lab 4
    Word document with code is in Assets folder as instructed

--- Assets/Script/GunComponent.cs
+++ Assets/Script/GunComponent.cs
@@ -4,10 +4,41 @@ public class GunComponent : MonoBehaviour
 {
     public GameObject bulletPrefab;
     public Transform bulletSpawnPoint;
-    public float bulletMaxImpulse = 10.0f;
+    public float bulletMaxImpulse = 100.0f;
     public float maxChargeTime = 3.0f;
     private float chargeTime = 0.0f;
     private bool isCharging = false;
 
+    void Update()
+    {
+    if (Input.GetButtonDown("Fire1"))
+    {
+        chargeTime = 0.0f;
+        isCharging = true;
+    }
+
+    if (Input.GetButton("Fire1"))
+    {
+        chargeTime += Time.deltaTime;
+        chargeTime = Mathf.Clamp(chargeTime, 0, maxChargeTime);
+    }
+
+    if (Input.GetButtonUp("Fire1"))
+    {
+        ShootBullet();
+        isCharging = false;
+    }
+    }
+
    
+    void ShootBullet()
+{
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+        Rigidbody rb = bullet.GetComponent<Rigidbody>();
+       
+        // Scale bullet force based on charge time
+        float bulletImpulse = (chargeTime / maxChargeTime) * bulletMaxImpulse;
+        rb.AddForce(bulletSpawnPoint.forward * bulletImpulse, ForceMode.Impulse);
+    }
+
 }
